d722004694be1899c72003aa7609a6e53a871248
PraneetPatare <praneetsp2001@gmail.com>
2022-09-18 15:06:58 +0530

    Updates

---
+++ precache-manifest.b7cbc7ec0c4e011a0bf9d0cc73f945ad.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a8ee8c94896296064e3afce2430acb",
+    "url": "/Keeper_App/index.html"
+  },
+  {
+    "revision": "4db286bc047176e313b0",
+    "url": "/Keeper_App/static/js/2.c6db2bb7.chunk.js"
+  },
+  {
+    "revision": "ee7eae8a5e9671d3b1a3",
+    "url": "/Keeper_App/static/js/main.fc452c44.chunk.js"
+  },
+  {
+    "revision": "444db0c95d67bc02296c",
+    "url": "/Keeper_App/static/js/runtime-main.8bb07458.js"
+  }
+]);
